cb923231a02ce0b7eea48656c60e63a97513d536
breelynbetts <30633493+breelynbetts@users.noreply.github.com>
2018-01-15 16:29:35 -0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/**
+  * File Name : Hello.java
+  * Purpose : Create first program that outputs "Hello, World!"
+  * @author : Breelyn Betts
+  * Date : 2018 - 00 - 16
+*/
+
+public class Hello {
+  public static void main( String[] args ) {
+    System.out.println( "Hello, world!" );
+ }
+}
