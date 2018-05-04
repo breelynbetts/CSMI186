2e0e8d784411e31c719573c5fbedd4fbd4363b88
breelynbetts <30633493+breelynbetts@users.noreply.github.com>
2018-05-04 01:08:23 -0700

    Add files via upload
    final project!

--- homework07/DynamicChangeMaker.java
+++ homework07/DynamicChangeMaker.java
@@ -29,73 +29,106 @@ public class DynamicChangeMaker {
   }
 
   public static Tuple makeChangeWithDynamicProgramming( int[] denoms, int targetValue) {
+    if (checkDupes(denoms)) {
+      return Tuple.IMPOSSIBLE;
+    }
+    if (validateTargetValue(targetValue)) {
+      return Tuple.IMPOSSIBLE;
+    }
+    if (validateDenoms(denoms)) {
+      return Tuple.IMPOSSIBLE;
+    }
+
     int length = denoms.length;
     int columns = targetValue + 1;
-
-    System.out.println("\ndenominations length: " + length + "\ntargetValue: " + targetValue);
-    if ( targetValue < 1 ) {
-      throw new IllegalArgumentException("Target Value must be greater than 0");
-    }
-
-    for ( int i = 0; i < length - 1; i++ ) {
-      for (int j = i + 1; j < length; j++ ) {
-        if ( denoms[i] == denoms[j] || denoms[i] < 1 || denoms[j] < 1 || denoms.length < 1 ) {
-          throw new IllegalArgumentException( "There may not be any duplicate denominations.");
-        }
-      }
-    }
-
     t = new Tuple[length][columns];
 
-    for ( int rows = 0; rows < length; rows++) {
-      for (int col = 0; col < columns; col++) {
-        if ( rows == 0 ) {
-          if ( col == 0 ) {
-            t[rows][col] = new Tuple(length);
-          } else if (denoms[rows] > col) {
-            t[rows][col] = Tuple.IMPOSSIBLE;
+    System.out.println("\ndenominations length: " + length + "\ntargetValue: " + targetValue);
+    fillWithZeroes(t, denoms);
+
+    for ( int i = 0; i < length; i++) {
+      for (int j = 0; j < columns; j++) {
+
+          if ( j == 0 ) {
+            t[i][j] = new Tuple(length);
+          } else if (denoms[i] > j) {
+            t[i][j] = Tuple.IMPOSSIBLE;
           } else {
-            t[rows][col] = new Tuple(length);
-            t[rows][col].setElement(rows, 1);
-            if ( t[rows][col - denoms[rows]].isImpossible() ) {
-              t[rows][col] = Tuple.IMPOSSIBLE;
+            t[i][j] = new Tuple(length);
+            t[i][j].setElement(i, 1);
+            if ( t[i][j - denoms[i]].isImpossible() ) {
+              t[i][j] = Tuple.IMPOSSIBLE;
             } else {
-              t[rows][col] = t[rows][col].add(t[rows][col - denoms[rows]]);
+              t[i][j] = t[i][j].add(t[i][j - denoms[i]]);
             }
           }
-        }
-        else {
-          if (col == 0) {
-            t[rows][col] = new Tuple(denoms.length);
-          }
-          if (denoms[rows] > col) {
-            t[rows][col] = Tuple.IMPOSSIBLE;
-            if ( !(t[rows-1][col].isImpossible()) ){
-              if ( t[rows-1][col].total() < t[rows][col].total()) {
-                t[rows][col] = t[rows-1][col];
+
+          if (i!= 0) {
+            if ( !(t[i-1][j].isImpossible()) ){
+              if ( t[i][j].isImpossible()) {
+                t[i][j] = t[i-1][j];
               }
-            }
-          } else {
-            t[rows][col] = new Tuple(denoms.length);
-            t[rows][col].setElement(rows, 1);
-            if ( t[rows][col - denoms[rows]].isImpossible() ) {
-              t[rows][col] = Tuple.IMPOSSIBLE;
-            } else {
-              t[rows][col] = t[rows][col].add(t[rows][col - denoms[rows]]);
-            } if ( !(t[rows-1][col].isImpossible()) ){
-              if ( t[rows-1][col].total() < t[rows][col].total()) {
-                t[rows][col] = t[rows-1][col];
+              if ( t[i-1][j].total() < t[i][j].total()) {
+                t[i][j] = t[i-1][j];
               }
             }
           }
-        }
+
       }
     }
     System.out.println("\t tuple: " + t[length-1][targetValue]);
 
-
     return t[length - 1][targetValue];
   }
+
+  /**
+   * method to check that each element is unique within the denominations array
+   * @param denom boolean value that evaluates each element of the integer array to
+   *   check for duplicate values
+   *   note the method displays true if there are duplicates
+   */
+
+  public static boolean checkDupes( int[] denoms ) {
+    for ( int i = 0; i < denoms.length - 1; i++ ) {
+      for (int j = i + 1; j < denoms.length; j++ ) {
+        if ( denoms[i] == denoms[j] || denoms[i] < 1 || denoms[j] < 1 || denoms.length < 1 ) {
+          return true;
+        }
+      }
+    } return false;
+  }
+
+
+  public static void fillWithZeroes( Tuple[][] t, int[] denom) {
+    for ( int i = 0; i < denom.length; i++ ) {
+      t[i][0] = new Tuple(denom.length);
+    }
+  }
+
+  /**
+   * method to check that target value is greater than 0
+   * @param target boolean value that evaluates the given target value and checks
+   *   for negative values
+   */
+  public static boolean validateTargetValue( int target ) {
+    if (target < 0) {
+      return true;
+    } return false;
+  }
+
+  /**
+   * method to check that each element of the denominations array is greater than 0
+   * @param denom boolean value that evaluates each element of the integer array to
+   *   check for negative values
+   */
+  public static boolean validateDenoms( int[] denom) {
+    for (int i = 0; i < denom.length; i++ ) {
+      if ( denom[i] < 1 ) {
+        return true;
+      }
+    } return false;
+  }
+
 
   public static void main ( String args[] ) {
     // only need for extra credit
